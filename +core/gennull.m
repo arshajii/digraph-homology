0ae13e93fe4472db2fb5cb43238d811ca53ee281
A. R. Shajii <ariya.r.shajii@gmail.com>
2013-02-25 19:33:17 -0500

    Optimized matrix generation in gennull.

--- +core/gennull.m
+++ +core/gennull.m
@@ -4,26 +4,16 @@ function A = gennull(boundarized, nonalloweds)
 % cancel). This function is used in OMEGA and DIMKER.
 
     A = zeros(size(nonalloweds,1), length(boundarized));
-    for i=1:size(nonalloweds,1)
-        v = nonalloweds(i,:);
-        
-        for j=1:length(boundarized)
-            c = 0;
-            generator = boundarized{j};
-            covered = zeros(1, size(generator,1));  % rows that we cover in next loop
+
+    for i=1:length(boundarized)
+        generator = boundarized{i};
+        for j=1:size(generator,1)
+            v = generator(j,:);
+            [~,indx] = ismember(v(2:end), nonalloweds, 'rows');
             
-            for k=1:size(generator,1)
-                if v == generator(k,2:end)
-                    c = c + generator(k,1);
-                    covered(k) = true;
-                else
-                    covered(k) = false;
-                end
+            if indx
+                A(indx, i) = A(indx, i) + v(1);
             end
-                    
-            A(i,j) = c;
-            generator(logical(covered),:) = [];  % delete covered rows (performance boost)
-            boundarized{j} = generator;  % reassign modified matrix
         end
     end
 end
