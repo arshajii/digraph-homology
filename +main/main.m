f46f737ab86b010a555c27fd9d7de91cf063ab30
A. R. Shajii <ariya.r.shajii@gmail.com>
2013-03-04 20:16:57 -0500

    Updated main file to include runs for the new Cayley graphs.

--- +main/main.m
+++ +main/main.m
@@ -1,1 +1,10 @@
-main.trial(20,1);
+for d=1:3
+    for p=2:10
+        fprintf('Z^%d mod %d\n', d, p);
+        
+        for i=1:5
+            dg = core.cayley(d,p);
+            disp(dg.hdim(1));
+        end
+    end
+end
